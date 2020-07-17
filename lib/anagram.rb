b1bcfb8360b4b0969f9f101cae1be428f16977c9
Micah Bowie <micahbowie20@gmail.com>
2020-07-17 00:00:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram 
+  attr_accessor :word   
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match (anagram_list)
+    split_up = anagram_list.split("").map { |s| s.split("") }
+    if split_up.sort == @word.sort
+      @word.sort
+    else Array.new
+    end 
+  end 
+end 
